ce4f0272e87a13029dbea091531d8b526c332ea8
naqeebghazi <naqeebghazi@outlook.com>
2023-11-04 21:56:16 +0000

    m

--- bashscriptexample.sh
+++ bashscriptexample.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-echo "Welcome to Linux"
+# echo "Welcome to Linux"
 
 # read -p "Enter your name please: " NAME
 
@@ -246,42 +246,145 @@ echo "Welcome to Linux"
 
 # ##############################################################
 
+#This creates a directory with two files in it then removes them while echoing every step of the script process
 
 #!/bin/bash
 
-# Display current directory
-echo "Current directory: $PWD"
+# # Display current directory
+# echo "Current directory: $PWD"
 
-# Create a new directory
-echo "Creating a new directory..."
-mkdir my_directory
-echo "New directory created."
+# # Create a new directory
+# echo "Creating a new directory..."
+# mkdir my_directory
+# echo "New directory created."
 
-# Change to the new directory
-echo "Changing to the new directory..."
-cd my_directory
-echo "Current directory: $PWD"
+# # Change to the new directory
+# echo "Changing to the new directory..."
+# cd my_directory
+# echo "Current directory: $PWD"
 
-# Create some files
+# # Create some files
+# echo "Creating files..."
+# touch file1.txt
+# touch file2.txt
+# echo "Files created."
+
+# # List the files in the current directory
+# echo "Files in the current directory:"
+# ls
+
+# # Move one level up
+# echo "Moving one level up..."
+# cd ..
+# echo "Current directory: $PWD"
+
+# # Remove the new directory and its contents
+# echo "Removing the new directory..."
+# rm -rf my_directory
+# echo "Directory removed."
+
+# # List the files in the current directory again
+# echo "Files in the current directory:"
+# ls
+
+
+# ##############################################################
+
+# File operations and sorting
+
+# #!/bin/bash
+
+# Create three files
 echo "Creating files..."
-touch file1.txt
-touch file2.txt
+echo "This is file3." > file3.txt
+echo "This is file1." > file1.txt
+echo "This is file2." > file2.txt
 echo "Files created."
 
-# List the files in the current directory
-echo "Files in the current directory:"
+# Display the files in their current order
+echo "Files in their current order:"
 ls
 
-# Move one level up
-echo "Moving one level up..."
-cd ..
-echo "Current directory: $PWD"
+# Sort the files alphabetically
+echo "Sorting files alphabetically..."
+ls | sort > sorted_files.txt
+echo "Files sorted."
 
-# Remove the new directory and its contents
-echo "Removing the new directory..."
-rm -rf my_directory
-echo "Directory removed."
+# Display the sorted files
+echo "Sorted files:"
+cat sorted_files.txt
 
-# List the files in the current directory again
-echo "Files in the current directory:"
-ls
+# Remove the original files
+echo "Removing original files..."
+rm file1.txt file2.txt file3.txt
+echo "Original files removed."
+
+# Rename the sorted file to a more descriptive name
+echo "Renaming sorted file..."
+mv sorted_files.txt sorted_files_sorted_alphabetically.txt
+echo "File renamed."
+
+# Display the final sorted file
+echo "Final sorted file:"
+cat sorted_files_sorted_alphabetically.txt
+
+
+# ##############################################################
+
+# Working with numbers
+
+#!/bin/bash
+
+# # Define two variables with numeric values
+# num1=10
+# num2=5
+
+# # Perform basic arithmetic operations
+# sum=$((num1 + num2))
+# difference=$((num1 - num2))
+# product=$((num1 * num2))
+# quotient=$((num1 / num2))
+# remainder=$((num1 % num2))
+
+# # Display the results
+# echo "Number 1: $num1"
+# echo "Number 2: $num2"
+# echo "Sum: $sum"
+# echo "Difference: $difference"
+# echo "Product: $product"
+# echo "Quotient: $quotient"
+# echo "Remainder: $remainder"
+
+# # Perform some more complex calculations
+# power_of_2=$((num1 ** 2))
+# square_root=$(awk "BEGIN{ sqrt=$num2; print sqrt }")
+
+# # Display the results
+# echo "Number 1 raised to the power of 2: $power_of_2"
+# echo "Square root of number 2: $square_root"
+
+
+# # ##############################################################
+
+# # File Backup
+
+# #!/bin/bash
+
+# # Define the source directory and backup directory
+# source_dir="/path/to/source_directory"
+# backup_dir="/path/to/backup_directory"
+
+# # Create a timestamp with the current date and time
+# timestamp=$(date +"%Y%m%d%H%M%S")
+
+# # Create a backup directory with the timestamp
+# backup_dir_with_timestamp="$backup_dir/backup_$timestamp"
+
+# # Create the backup directory
+# mkdir -p "$backup_dir_with_timestamp"
+
+# # Copy all files from the source directory to the backup directory
+# cp -r "$source_dir"/* "$backup_dir_with_timestamp"
+
+# # Display a message indicating the backup process is complete
+# echo "Backup completed. Files copied to: $backup_dir_with_timestamp"
